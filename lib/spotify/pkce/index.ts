3a9e34dd2798af1e52658a3c51b0e3a02afd60bd
fwttnnn <lolywk@tutanota.com>
2025-11-24 09:19:19 +0700

    chore: whoops, uh

--- lib/spotify/pkce/index.ts
+++ lib/spotify/pkce/index.ts
@@ -21,7 +21,7 @@ export const generateCodeVerifier = (length: number): string => {
   let text = ""
   let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
 
-  for (let i = 0 i < length i++) {
+  for (let i = 0; i < length; i++) {
     text += possible.charAt(Math.floor(Math.random() * possible.length))
   }
 
