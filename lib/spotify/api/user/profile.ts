9d1c4ef1d1da9e196de380ff8bed38d4695b7e4b
fwttnnn <lolywk@tutanota.com>
2025-11-28 20:36:44 +0700

    fix: safeguard for profile image instead

--- lib/spotify/api/user/profile.ts
+++ lib/spotify/api/user/profile.ts
@@ -16,5 +16,10 @@ export default async (token: string): Promise<{[key: string]: any}> => {
   console.log("profile --result:", result)
   const json = await result.json()
   console.log("profile:", json)
-  return json
+
+  const imageURL = json.images && json.images.length > 0
+    ? json.images[0].url
+    : "https://i.pinimg.com/736x/68/fd/b0/68fdb089f4f34de82fd6d18f00b95f1e.jpg"
+
+  return {...json, images: [{ url: imageURL }]}
 }
